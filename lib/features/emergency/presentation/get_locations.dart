ba9312eead80e37ec3a6e7a17c53e4dbd2aa0393
Ishaan Singh <asishaan.191cs124@nitk.edu.in>
2021-10-17 16:57:10 +0530

    Added beauty to locations.dart

--- lib/features/emergency/presentation/get_locations.dart
+++ lib/features/emergency/presentation/get_locations.dart
@@ -12,62 +12,71 @@ class _GetLocationsState extends State<GetLocations> {
     Location location = new Location();
     final Future<dynamic> _hash = location.getLocations();
     return Scaffold(
-        body: DefaultTextStyle(
-      style: Theme.of(context).textTheme.headline2!,
-      textAlign: TextAlign.center,
-      child: FutureBuilder<dynamic>(
-        future: _hash, // a previously-obtained Future<String> or null
-        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          List<Widget> children;
-          if (snapshot.hasData) {
-            children = <Widget>[];
-            snapshot.data?.forEach((key, value) {
-              children.add(
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 5.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      labelText: "${key}: ${value}",
+      appBar: AppBar(
+        title: const Text('Emergency Locations'),
+        backgroundColor: Color.fromRGBO(181, 7, 23, 1),
+      ),
+      body: FutureBuilder<dynamic>(
+          future: _hash, // a previously-obtained Future<String> or null
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            List<Widget> children;
+            if (snapshot.hasData) {
+              children = <Widget>[];
+              snapshot.data?.forEach((key, value) {
+                children.add(Container(
+                  margin: const EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(5.0),
+                  decoration: new BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    boxShadow: [
+                      new BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 5.0,
+                      ),
+                    ],
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Color.fromRGBO(181, 7, 23, 1),
+                    ),
+                    title: Text(
+                      value,
                     ),
                   ),
+                ));
+              });
+            } else if (snapshot.hasError) {
+              children = <Widget>[
+                const Icon(
+                  Icons.error_outline,
+                  color: Color.fromRGBO(181, 7, 23, 1),
+                  size: 60,
                 ),
-              );
-            });
-          } else if (snapshot.hasError) {
-            children = <Widget>[
-              const Icon(
-                Icons.error_outline,
-                color: Colors.red,
-                size: 60,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child: Text('Error: ${snapshot.error}'),
-              )
-            ];
-          } else {
-            children = const <Widget>[
-              SizedBox(
-                child: CircularProgressIndicator(),
-                width: 60,
-                height: 60,
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 16),
-                child: Text('Awaiting result...'),
-              )
-            ];
-          }
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
+                Padding(
+                  padding: const EdgeInsets.only(top: 2),
+                  child: Text('Error: ${snapshot.error}'),
+                )
+              ];
+            } else {
+              children = const <Widget>[
+                SizedBox(
+                  child: CircularProgressIndicator(),
+                  width: 60,
+                  height: 60,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Text('Awaiting result...'),
+                )
+              ];
+            }
+            return ListView(
+              controller: ScrollController(),
+              padding: const EdgeInsets.symmetric(vertical: 15),
               children: children,
-            ),
-          );
-        },
-      ),
-    ));
+            );
+          }),
+    );
   }
 }
