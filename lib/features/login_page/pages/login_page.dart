161b96ea7b845cbc432f0b61fc086e7041dc27a5
aakarshee2000 <pradeepartigmailcom.191cs102@nitk.edu.in>
2021-10-17 20:42:31 +0400

    Final Login
    Signed-off-by: aakarshee2000 <pradeepartigmailcom.191cs102@nitk.edu.in>

--- lib/features/login_page/pages/login_page.dart
+++ lib/features/login_page/pages/login_page.dart
@@ -109,7 +109,7 @@ class _LoginPage extends State<LoginPage> {
 	                    ),
 	                  )),
 	                  SizedBox(height: 70,),
-	                  FadeAnimation(1.5, Text("Forgot Password?", style: TextStyle(color: Color.fromRGBO(143, 148, 251, 1)),)),
+	                 
 	                ],
 	              ),
 	            )
