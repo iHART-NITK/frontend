6a102e32bde63149a0b0c256f5b3c85ae832de04
Nishant Nayak <nishantnayak2001@gmail.com>
2021-10-17 14:25:35 +0530

    Fixed the DjangoApp class to return only Response

--- lib/core/network/django_app.dart
+++ lib/core/network/django_app.dart
@@ -1,18 +1,21 @@
-import 'dart:convert' as convert;
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 class DjangoApp {
-  Future<dynamic> get(url, {user_specific = true}) async {
-    var addr;
-    if (user_specific)
-      addr = Uri.parse('http://localhost:3000/user/${2}${url}');
+  Future<http.Response> get(url, {userSpecific = true}) async {
+    var _addr;
+    String host = "127.0.0.1";
+    String port = "8000";
+
+    int userID = 2;
+    String token = "265bcfe1d1e933e38b00e9c28c4ee42a8e765156";
+    if (userSpecific)
+      _addr = Uri.parse('http://$host:$port/user/$userID/$url');
     else
-      addr = Uri.parse('http://localhost:3000${url}');
-    var response = await http.get(addr, headers: {
-      'Authorization': 'Token 265bcfe1d1e933e38b00e9c28c4ee42a8e765156'
-    });
-    var resp = convert.jsonDecode(response.body) as Map<String, dynamic>;
-    print(resp);
-    return (resp);
+      _addr = Uri.parse('http://$host:$port/$url');
+    var response =
+        await http.get(_addr, headers: {'Authorization': 'Token $token'});
+    debugPrint("[API REQ] $_addr ${response.statusCode}");
+    return response;
   }
 }
--- lib/features/emergency/data/model/location_model.dart
+++ lib/features/emergency/data/model/location_model.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+
 class LocationModel {
-  Map<String, String> get_location_objects_from_json(Map<String, dynamic> j) {
-    print(j);
-    print(j.runtimeType);
+  Map<String, String> get_location_objects_from_json(Response response) {
+    print(response);
+    print(response.runtimeType);
     Map<String, String> locations = new Map();
+    Map<String, dynamic> j = jsonDecode(response.body) as Map<String, dynamic>;
     j.forEach((key, value) {
       print("${key}: ${value}");
       locations[key] = value;
