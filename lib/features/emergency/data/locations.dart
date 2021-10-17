28abb2ac2e7bf943dd30c4382188bbfbaabd28cf
Ishaan Singh <asishaan.191cs124@nitk.edu.in>
2021-10-17 14:25:56 +0530

    Corrected django_app

--- lib/features/emergency/data/locations.dart
+++ lib/features/emergency/data/locations.dart
@@ -1,13 +1,15 @@
+import 'dart:convert' as convert;
 import 'package:frontend/features/emergency/data/model/location_model.dart';
 import '../../../core/network/django_app.dart';
 
 class Location {
   Future<dynamic> getLocations() async {
-    final response = await DjangoApp().get('/api/emergency/locations');
-    print(response);
+    final response =
+        await DjangoApp().get('/emergency/locations', user_specific: false);
+    var location_hash =
+        convert.jsonDecode(response.body) as Map<String, dynamic>;
     LocationModel m = new LocationModel();
-    dynamic x = m.get_location_objects_from_json(response);
-    print(x);
-    return response;
+    dynamic x = m.get_location_objects_from_json(location_hash);
+    return x;
   }
 }
--- lib/features/emergency/data/model/location_model.dart
+++ lib/features/emergency/data/model/location_model.dart
@@ -1,13 +1,9 @@
 class LocationModel {
   Map<String, String> get_location_objects_from_json(Map<String, dynamic> j) {
-    print(j);
-    print(j.runtimeType);
     Map<String, String> locations = new Map();
     j.forEach((key, value) {
-      print("${key}: ${value}");
       locations[key] = value;
     });
-    print(locations.keys);
     return locations;
   }
 }
