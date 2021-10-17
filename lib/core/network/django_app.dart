b6a4d99bf7d74983f84a1fccd1ef5ae625dcf1b2
Ishaan Singh <asishaan.191cs124@nitk.edu.in>
2021-10-17 13:57:16 +0530

    User apecific resource access added

--- lib/core/network/django_app.dart
+++ lib/core/network/django_app.dart
@@ -2,8 +2,12 @@ import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
 class DjangoApp {
-  Future<dynamic> get(url) async {
-    var addr = Uri.parse('http://localhost:3000${url}');
+  Future<dynamic> get(url, {user_specific = true}) async {
+    var addr;
+    if (user_specific)
+      addr = Uri.parse('http://localhost:3000/user/${2}${url}');
+    else
+      addr = Uri.parse('http://localhost:3000${url}');
     var response = await http.get(addr, headers: {
       'Authorization': 'Token 265bcfe1d1e933e38b00e9c28c4ee42a8e765156'
     });
