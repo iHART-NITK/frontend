84639deddecbd6e8058d91aae240b2e208393e34
Nishant Nayak <nishantnayak2001@gmail.com>
2021-10-26 11:18:44 +0530

    Update Auth to get tokens on subsequent requests

--- lib/core/network/django_app.dart
+++ lib/core/network/django_app.dart
@@ -3,11 +3,12 @@ import 'package:hive/hive.dart';
 import 'package:http/http.dart' as http;
 
 class DjangoApp {
+  String host = "localhost";
+  String port = "3000";
+
   Future<http.Response> get(url, {userSpecific = false}) async {
     var _addr;
     var box = Hive.box('user');
-    String host = "localhost";
-    String port = "3000";
 
     int userId = box.get(0).id;
     String token = box.get(0).token;
@@ -24,8 +25,6 @@ class DjangoApp {
   Future<http.Response> post({url, data}) async {
     var _addr;
     var box = Hive.box('user');
-    String host = "localhost";
-    String port = "3000";
 
     String token = box.get(0).token;
     _addr = Uri.parse('http://$host:$port/api$url');
@@ -37,8 +36,6 @@ class DjangoApp {
 
   Future<http.Response> postAnonymous({url, data}) async {
     var _addr;
-    String host = "localhost";
-    String port = "3000";
 
     _addr = Uri.parse('http://$host:$port/api$url');
     var response = await http.post(_addr, body: data);
