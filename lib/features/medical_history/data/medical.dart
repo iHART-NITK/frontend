cad6ec2f253d82e1ad3a6011bee3310c6df469d5
Ikjot Singh <ikjotsd@gmail.com>
2021-11-08 21:37:59 +0530

    qr code

--- lib/features/medical_history/data/medical.dart
+++ lib/features/medical_history/data/medical.dart
@@ -1,11 +1,31 @@
+import 'dart:convert';
+
 import 'package:frontend/features/medical_history/data/model/medical_history_model.dart';
 import '../../../core/network/django_app.dart';
+import 'package:crypto/crypto.dart';
+import 'package:hive/hive.dart';
+
 import 'dart:convert' as convert;
 
 class History {
   Future<dynamic> getMed() async {
     final response =
         await DjangoApp().get('/medical-history', userSpecific: true);
+    print(response);
+    var medicalHash = convert.jsonDecode(response.body) as List<dynamic>;
+    HistoryModel m = new HistoryModel();
+    dynamic x = m.getLocationObjectsFromJson(medicalHash);
+    print(x);
+    return x;
+  }
+
+  Future<dynamic> getQR() async {
+    Hive.openBox('user');
+    var box = Hive.box('user');
+    var bytes1 = utf8.encode(box.get(0).token);
+    var digest1 = sha256.convert(bytes1);
+    final response = await DjangoApp()
+        .get('/medical-history/html?token=$digest1', userSpecific: true);
     print(response);
     var medicalHash = convert.jsonDecode(response.body) as List<dynamic>;
     HistoryModel m = new HistoryModel();
---
+++ lib/features/medical_history/presentation/medical_history_qr_page.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:crypto/crypto.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:hive/hive.dart';
+import 'package:http/http.dart' as http;
+
+class MedicalHistoryQRPage extends StatefulWidget {
+  const MedicalHistoryQRPage({Key? key}) : super(key: key);
+
+  @override
+  _MedicalHistoryQRPageState createState() => _MedicalHistoryQRPageState();
+}
+
+class _MedicalHistoryQRPageState extends State<MedicalHistoryQRPage> {
+  Future<String> getQR() async {
+    Hive.openBox('user');
+    var box = Hive.box('user');
+    var bytes1 = utf8.encode(box.get(0).token);
+    var digest1 = sha256.convert(bytes1);
+    String userId = box.get(0).id.toString();
+    final response =
+        await http.post(Uri.parse('http://localhost:3000/'), body: {
+      "data":
+          "http://localhost:8000/api/user/$userId/medical-history/html?token=${digest1.toString()}",
+      "ecl": "L"
+    });
+    Map<String, dynamic> jsonSvg = jsonDecode(response.body);
+    return jsonSvg["final_svg"];
+    // DrawableRoot svgRoot =
+    //     await svg.fromSvgString(jsonSvg["final_svg"], jsonSvg["final_svg"]);
+    // print('H1');
+    // final directory = await getApplicationDocumentsDirectory();
+    // print('H1');
+    // print(directory);
+    // final Picture picture = svgRoot.toPicture();
+    // return (svgRoot.toPicture().toImage(500, 500));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QR'),
+        backgroundColor: Colors.red,
+      ),
+      body: FutureBuilder(
+        future: getQR(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          } else if (snapshot.hasData) {
+            String svgFound = snapshot.data.toString();
+            print(svgFound);
+            int index = svgFound.indexOf('width="100%" height="100%"');
+            print(index);
+            String s1 = svgFound.substring(0, index);
+            int newindex = s1.indexOf("<svg");
+            s1 = s1.substring(0, newindex + 4) +
+                ' width="500px" height="500px"' +
+                s1.substring(newindex + 4);
+            String s2 = svgFound.substring(index + 27);
+            print(s1 + s2);
+            return Html(data: s1 + s2);
+          } else
+            return Center(child: Text('There was some error!'));
+        },
+      ),
+    );
+  }
+}
